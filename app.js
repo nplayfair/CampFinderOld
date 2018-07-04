44facb9cab1b673508b1792a8ba81de7935a1f30
Nick Playfair <nplayfair@protonmail.com>
2018-07-04 19:03:34 +0100

    Form to add new campground

--- app.js
+++ app.js
@@ -5,24 +5,34 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7381/9705573948_3f342901d1.jpg"},
+  {name: "Granite Hill", image: "https://farm1.staticflickr.com/82/225912054_690e32830d.jpg"},
+  {name: "Crantock Bay", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
+];
+
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7381/9705573948_3f342901d1.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/82/225912054_690e32830d.jpg"},
-    {name: "Crantock Bay", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
-  ]
+
 
   res.render("campgrounds", {campgrounds: campgrounds});
 });
 
-app.post("/campgrounds", function(res, req) {
+app.post("/campgrounds", function(req, res) {
   //get data from form and add to campgrounds array
-
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image}
+  campgrounds.push(newCampground);
   //redirect back to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs");
 });
 
 //Tell express to listen for requests
