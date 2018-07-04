d3e87c9e479f81d4500ce184a41e6b11e911a093
Nick Playfair <nplayfair@protonmail.com>
2018-07-04 18:37:01 +0100

    Display list of campgrounds from an array

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res) {
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7381/9705573948_3f342901d1.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/82/225912054_690e32830d.jpg"},
+    {name: "Crantock Bay", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
+  ]
+
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
 //Tell express to listen for requests
 app.listen(3000, function(){
   console.log("Server started.");
