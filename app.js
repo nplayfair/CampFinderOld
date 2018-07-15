cd2e01656b5a7c08761a7cc7ea45e96f0f4c881e
Nick Playfair <nick@chaosemerald.co.uk>
2018-07-15 20:20:05 +0100

    Add comment functionality and views

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
     seedDB      = require("./seeds")
 
 mongoose.connect("mongodb://127.0.0.1:27017/campfinder", {useNewUrlParser: true});
@@ -73,7 +74,41 @@ app.get("/campgrounds/:id", function(req, res) {
 //=================
 
 app.get("/campgrounds/:id/comments/new", function(req, res) {
-  res.render("comments/new");
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  //Look up campground using id
+    Campground.findById(req.params.id, function(err, campground) {
+      if(err) {
+        console.log(err);
+        res.redirect("/campgrounds");
+      }
+      else {
+        Comment.create(req.body.comment, function(err, comment) {
+          if(err) {
+            console.log(err);
+          }
+          else {
+            campground.comments.push(comment);
+            campground.save();
+            res.redirect("/campgrounds/" + campground._id);
+          }
+        });
+      }
+    })
+    //Create new comment
+    //Associate comment with campground
+    //Redirect
+
+
 });
 
 
