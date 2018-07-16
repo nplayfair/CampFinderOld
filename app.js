3db0a70a6038c8c320a2759b132a45227496c66d
Nick Playfair <nick@chaosemerald.co.uk>
2018-07-16 14:07:55 +0100

    serve public dir and add some more styling

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express     = require("express"),
 mongoose.connect("mongodb://127.0.0.1:27017/campfinder", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 //Routes
