a2fa4f45e10ab2c1ca05db6ca8a32b2a5f75c0f2
Nick Playfair <nick@nickplayfair.co.uk>
2018-07-30 15:58:05 +0100

    adjust app config ready for deployment
    specify env port and create npm start script

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
 //Tell express to listen for requests
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Server started.");
 });
